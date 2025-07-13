5c3e3770d41e78a71997e964f7dfa9e2d5f51bcf
Antonio Labinjan <alabinjan6@gmail.com>
2025-07-13 10:08:12 +0200

    Create send.py

---
+++ send.py
@@ -0,0 +1,13 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='my_queue')
+
+channel.basic_publish(exchange='',
+                      routing_key='my_queue',
+                      body='TestTest!')
+
+print(" [x] Sent 'Hello RabbitMQ!'")
+connection.close()
